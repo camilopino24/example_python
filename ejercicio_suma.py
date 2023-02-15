918a0cd89c62efb626c7fa413cae385e8e4c89be
camilopino24 <cpc.camilo.13@gmail.com>
2023-02-15 17:33:16 +0100

    se agrego una multiplicacion

---
+++ ejercicio_suma.py
@@ -0,0 +1,12 @@
+number_one = 6
+number_two = 3
+
+#operacion de sumar dos numeros 
+
+suma = number_one + number_two 
+resta = number_one -number_two
+multiplicacion = number_one * number_two
+
+print (resta)
+print(suma)
+print (multiplicacion)
